def52b64cdf37da205b8bd94fcc20f2287fd133d
HenryWHM <whm2903@gmail.com>
2025-02-19 23:40:13 +1100

    Laalal

--- src/MediaList.jsx
+++ src/MediaList.jsx
@@ -9,7 +9,11 @@ import { useNavigate } from 'react-router-dom';
 const Media = ({imgSrc, title, description, Alt, navigateTo}) => {
     const navigate = useNavigate();
     const handleClick = () => {
-        navigate(navigateTo);
+        if (navigateTo.startsWith("http")) {
+            window.open(navigateTo, "_blank");  // Open external links in a new tab
+        } else {
+            navigate(navigateTo);  // Navigate within the React app
+        }
     };
 
     return (
@@ -27,35 +31,35 @@ const MediaList = () => {
             title: "Facebook 🇰🇷",
             description: "Click to follow!",
             alt: "Facey",
-            navigateTo: ""
+            navigateTo: "https://www.facebook.com/profile.php?id=100008810344712"
         },
         {
            imgSrc: instaIcon,
            title: "Instagram 🇯🇵",
            description: "Click to follow!",
            alt: "Insta",
-           navigateTo: "/hobbies"
+           navigateTo: "https://www.instagram.com/cannot_be_hm"
         },
         {
             imgSrc: discordIcon,
             title: "Discord 🇨🇳",
             description: "Click to follow!",
             alt: "Discey",
-            navigateTo: "/projects"
+            navigateTo: "https://discordapp.com/users/584605039046295563"
         },
         {
             imgSrc: githubIcon,
             title: "Github 🇸🇬",
             description: "Click to follow!",
             alt: "Facey for Comp Sci nerds",
-            navigateTo: ""
+            navigateTo: "https://github.com/HenryWHM"
         }
 
     ]
     return (
         <div className="media-container">
           {medias.map((media, index) => (
-            <Media key={index} {...card} />
+            <Media key={index} {...media} />
           ))}
         </div>
     );
--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,1 +1,15 @@
 import React from 'react';
+import MediaList from '../MediaList.jsx';
+
+const Contacts = () => {
+    return (
+        <div className="App">
+            <div className="d-flex w-100 card-container">
+                <MediaList />
+            </div>
+        </div>
+    )
+}
+
+export default Contacts;
+
